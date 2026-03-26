a71331003ef5bc5f102edc00a8627d4d658e1cc5
Helmifran Gonzaga <contato@helmifran.com>
2026-03-26 19:31:34 -0300

    fix: corrigir expressões regulares em jest.config.js para uso de String.raw

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,13 +2,13 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
   moduleDirectories: ['node_modules', 'src'],
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  transformIgnorePatterns: [String.raw`node_modules/(?!.*\.mjs$)`],
   transform: {
     '^.+\\.(ts|js|mjs|html|svg)$': [
       'jest-preset-angular',
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
+        stringifyContentPathRegex: String.raw`\.(html|svg)$`,
         diagnostics: false,
       },
     ],
