2e7d43ab9c18e1b31096d2b828ea1d4185b1d7cd
Helmifran Gonzaga <contato@helmifran.com>
2026-03-26 19:31:27 -0300

    feat: adicionar mapeamento para ignorar id e versão em métodos do BeneficioMapper

--- backend-module/src/main/java/com/example/backend/adapter/inbound/web/mapper/BeneficioMapper.java
+++ backend-module/src/main/java/com/example/backend/adapter/inbound/web/mapper/BeneficioMapper.java
@@ -4,15 +4,20 @@ import com.example.backend.adapter.inbound.web.dto.BeneficioRequest;
 import com.example.backend.adapter.inbound.web.dto.BeneficioResponse;
 import com.example.backend.domain.model.Beneficio;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface BeneficioMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
     Beneficio toDomain(BeneficioRequest request);
 
     BeneficioResponse toResponse(Beneficio beneficio);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "version", ignore = true)
     void updateDomainFromRequest(BeneficioRequest request, @MappingTarget Beneficio beneficio);
 }
